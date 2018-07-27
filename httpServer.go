72f14e71e48d5b39aa903515924bf0eec4880f9a
Paulo Fortes <pauloffn@gmail.com>
2018-07-27 16:15:02 -0300

    legals

--- httpServer.go
+++ httpServer.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type EprocessoData struct {
@@ -130,6 +131,68 @@ func deleteFilesHandler(pasta_down string) http.HandlerFunc {
 	}
 }
 
+func initSidaHandler(api *apiConn) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			api.SIDAInit()
+			w.Header().Set("Content-Type", "application/json")
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Write([]byte("ok"))
+		} else {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		}
+	}
+}
+
+func pesquisaSidaProcesso(api *apiConn) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, "Error reading request body",
+					http.StatusInternalServerError)
+			}
+			processo := string(body)
+			Trace.Println("processo no handler (body)\n", processo)
+			resp := api.SIDAConsultaProcesso(processo)
+			w.Header().Set("Content-Type", "application/json")
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Write([]byte(resp))
+		} else {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		}
+	}
+}
+
+func pesquisaSidaVariosProcessos(api *apiConn) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		(w).Header().Set("Access-Control-Allow-Origin", "*")
+		(w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		(w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		if r.Method == "POST" {
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, "Error reading request body",
+					http.StatusInternalServerError)
+			}
+			processos := string(body)
+			Trace.Println("processos no handler (body)\n", processos)
+			arrProcs := strings.Split(processos, "|")
+			respostaFinal := ""
+			api.SIDAInit()
+			for _, processo := range arrProcs {
+				// element is the element from someSlice for where we are
+				resp := api.SIDAConsultaProcesso(processo)
+				respostaFinal = respostaFinal + "###" + processo + "$$$" + resp
+			}
+
+			w.Write([]byte(respostaFinal))
+		} else {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		}
+	}
+}
+
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -148,9 +211,12 @@ func serveHttp(api *apiConn, pdfPath string, port string) {
 	http.Handle("/pdf/", corsMiddleware(http.StripPrefix("/pdf/", fsPdf)))
 	Trace.Printf("\n-x")
 	// Trace.Printf("\n\nDATA (serve api)%#v\n---\n-< do serveApi", data)
-	http.HandleFunc("/", handlerWithInitialTemplate(api, pdfPath, port)) // set router
-	http.HandleFunc("/json", handlerWithInitialJson(api, pdfPath))       // set router
-	http.HandleFunc("/deletefiles", deleteFilesHandler(pdfPath))         // set router
+	http.HandleFunc("/", handlerWithInitialTemplate(api, pdfPath, port))                 // set router
+	http.HandleFunc("/json", handlerWithInitialJson(api, pdfPath))                       // set router
+	http.HandleFunc("/deletefiles", deleteFilesHandler(pdfPath))                         // set router
+	http.HandleFunc("/initSida", initSidaHandler(api))                                   // set router
+	http.HandleFunc("/pesquisa_sida_processo", pesquisaSidaProcesso(api))                // set router
+	http.HandleFunc("/pesquisa_sida_varios_processos", pesquisaSidaVariosProcessos(api)) // set router
 	Info.Printf("\nServir na porta " + port + "... Visite http://localhost:" + port + " no Chrome (ou Firefox se tiver atualizado)")
 	Trace.Printf("\n-x")
 	err := http.ListenAndServe(":"+port, nil) // set listen port
