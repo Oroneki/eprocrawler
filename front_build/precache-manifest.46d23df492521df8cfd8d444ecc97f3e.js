9c6a39c1c9b10b4440156a0c5738d738ac4154a8
Paulo Fortes <pauloffn@gmail.com>
2019-03-19 14:11:04 -0300

    tela horizontal front

---
+++ front_build/precache-manifest.46d23df492521df8cfd8d444ecc97f3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "937eada5b18066f0f732",
+    "url": "/static/js/main.dae44c8d.chunk.js"
+  },
+  {
+    "revision": "8a7354092f942a796db7",
+    "url": "/static/js/2.d8864de7.chunk.js"
+  },
+  {
+    "revision": "937eada5b18066f0f732",
+    "url": "/static/css/main.2f3c742c.chunk.css"
+  },
+  {
+    "revision": "8a7354092f942a796db7",
+    "url": "/static/css/2.7c8c438d.chunk.css"
+  },
+  {
+    "revision": "0caaff87118b8af490575cacf61f6ec9",
+    "url": "/index.html"
+  }
+];
---
+++ front_build/precache-manifest.9045d86486d46060619204f9dfb45f0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "828ab600516a4c43ce6b",
+    "url": "/static/js/main.adcc8658.chunk.js"
+  },
+  {
+    "revision": "8a7354092f942a796db7",
+    "url": "/static/js/2.d8864de7.chunk.js"
+  },
+  {
+    "revision": "828ab600516a4c43ce6b",
+    "url": "/static/css/main.e29b5a74.chunk.css"
+  },
+  {
+    "revision": "8a7354092f942a796db7",
+    "url": "/static/css/2.7c8c438d.chunk.css"
+  },
+  {
+    "revision": "76315e38736dfe865373bd73c1267ec5",
+    "url": "/index.html"
+  }
+];
